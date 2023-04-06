3261a02b6726c4c3c5952004d8017948f68e8987
Celso Bertolotto <celsonyl@hotmail.com>
2023-04-06 14:16:54 -0300

    feat: create category

--- src/app/core/services/category.service.ts
+++ src/app/core/services/category.service.ts
@@ -2,14 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import Category from '../models/category.model';
+import { KeycloakService } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
   private url = 'http://localhost:8181';
+  private tokenKeycloak!: string;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly keycloak: KeycloakService
+  ) {
+    this.keycloak.getToken().then((res) => {
+      this.tokenKeycloak = res;
+    });
+  }
+
+  private mountHeaders() {
+    return {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${this.tokenKeycloak}`,
+    };
+  }
 
   getcategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${this.url}/category`);
@@ -17,5 +33,11 @@ export class CategoryService {
 
   getCategoryById(id: string): Observable<Category> {
     return this.http.get<Category>(`${this.url}/category/${id}`);
+  }
+
+  createCategory(category: Category) {
+    return this.http.post(`${this.url}/category`, category, {
+      headers: this.mountHeaders(),
+    });
   }
 }
--- src/app/pages/catalog-administration/pages/create-edit-category/create-edit-category.component.ts
+++ src/app/pages/catalog-administration/pages/create-edit-category/create-edit-category.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import Category from 'src/app/core/models/category.model';
+import { CategoryService } from 'src/app/core/services/category.service';
 
 @Component({
   selector: 'app-create-edit-category',
   templateUrl: './create-edit-category.component.html',
   styleUrls: ['./create-edit-category.component.scss']
 })
-export class CreateEditCategoryComponent {
+export class CreateEditCategoryComponent implements OnInit {
 
+  loading = true;
+  form!: FormGroup;
+  isEdition = false;
+  categoryId!: string;
+  category: Category = {
+    id: 0,
+    image: '',
+    name: '',
+  };
+
+  constructor(
+    private categoryService: CategoryService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.categoryId = params['id'];
+    });
+
+    if (this.categoryId) {
+      this.isEdition = true;
+      this.categoryService.getCategoryById(this.categoryId).subscribe({
+        next: (res) => {
+          this.category = res;
+          this.createForm();
+          this.loading = false;
+        },
+      });
+      return;
+    }
+
+    this.createForm();
+    this.loading = false;
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      name: [this.category.name || '', Validators.required],
+      image: [this.category.image || '', Validators.required]
+    })
+  }
+
+  submitCategory() {
+    if (this.isEdition) return;
+
+    this.categoryService.createCategory(this.form.getRawValue()).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Categoria criada com sucesso',
+          life: 3000,
+        });
+        this.router.navigate(['/catalog-administration']);
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao criar a categoria.',
+          life: 3000,
+        });
+      },
+    });
+  }
 }
